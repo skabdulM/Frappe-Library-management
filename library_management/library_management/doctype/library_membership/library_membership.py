d9cadef2fcb12320a4f2722ba9c3ff8c3861544f
Abdul Mannan Shaikh <abdulmannan8334@gmail.com>
2024-06-24 11:13:49 +0000

    Added Library membership added and function to check if there  is and active membership

--- library_management/library_management/doctype/library_membership/library_membership.py
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2024, skabdulM and contributors
 # For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
 from frappe.model.docstatus import DocStatus
